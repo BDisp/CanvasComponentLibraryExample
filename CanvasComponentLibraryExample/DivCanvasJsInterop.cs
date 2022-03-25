3cc43809c3d3a5c9327ab53c91a57f5451adbe75
BDisp <bd.bdisp@gmail.com>
2022-03-25 00:06:33 +0000

    Add project files.

---
+++ CanvasComponentLibraryExample/DivCanvasJsInterop.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.JSInterop;
+
+namespace CanvasComponentLibraryExample
+{
+    //public class DivCanvasJsInterop
+    //{
+    //    public const string JsGetDivCanvasOffset = "divCanvasJsFunctions.getDivCanvasOffset";
+
+    //    public static ValueTask<string> GetDivCanvasOffset(IJSRuntime jsRuntime, object[] divCanvas)
+    //    {
+    //        // Implemented in divCanvasJsInterop.js
+    //        return jsRuntime.InvokeAsync<string>(
+    //            JsGetDivCanvasOffset,
+    //            divCanvas);
+    //    }
+    //}
+
+    public class DivCanvasJsInterop : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public DivCanvasJsInterop(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+               "import", "./_content/CanvasComponentLibraryExample/divCanvasJsInterop.js").AsTask());
+        }
+
+        public async ValueTask<string> GetDivCanvasOffset(object[] divCanvas)
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<string>("getDivCanvasOffset", divCanvas);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ CanvasComponentLibraryExample/wwwroot/divCanvasJsInterop.js
@@ -0,0 +1,19 @@
+﻿//import "_content/Blazor.Extensions.Canvas/blazor.extensions.canvas.js"
+//import "./_content/Blazor.Extensions.Canvas/blazor.extensions.canvas.js"
+//import "blazor.extensions.canvas.js"
+
+//window.divCanvasJsFunctions = {
+//    getDivCanvasOffset: function (el) {
+//        var obj = {};
+//        obj.offsetLeft = el.offsetLeft;
+//        obj.offsetTop = el.offsetTop;
+//        return JSON.stringify(obj);
+//    }
+//};
+
+export function getDivCanvasOffset(el) {
+    var obj = {};
+    obj.offsetLeft = el.offsetLeft;
+    obj.offsetTop = el.offsetTop;
+    return JSON.stringify(obj);
+}
